72961bafe89c4748818be84948636158a6895929
Saimunjd <saimuncoder@gmail.com>
2023-06-20 23:26:24 +0600

    login and register system in portfolio

--- testdjango/main/models.py
+++ testdjango/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class SocialHandles(models.Model):
+    social_id = models.CharField(max_length=100, default='blank')
+    social_link=models.CharField(max_length=500, default='blank')
+    
--- testdjango/main/views.py
+++ testdjango/main/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import SocialHandles
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    profiles = SocialHandles.objects.all()
+    return render(request, 'index.html', {'profiles': profiles})
 
-def counter(request):
-    text = request.GET['text']
-    wordlen = len(text.split())
-    return render(request, 'counter.html', {'wordlen': wordlen})
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+        if password == repeat_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'password didn\'t match')
+            return redirect('register')             
+    else:
+        return render(request, 'register.html')
+            
+
+    return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials not found.')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
